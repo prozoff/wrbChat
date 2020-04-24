e228d26802d9d114ba2fba1684c0b84a1efdaaf1
prozoff <55830635+prozoff@users.noreply.github.com>
2020-04-24 15:04:36 +0300

    1
    1

---
+++ WebChat/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebChat
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            await this.Clients.All.SendAsync("Send", message, userName);
+
+        }
+
+        public async Task editText(string message, string id, string userName)
+        {
+            await this.Clients.All.SendAsync("editText", message, id, userName);
+        }
+
+        public async Task sendNewName(string oldName, string newName)
+        {
+            await this.Clients.All.SendAsync("sendNewName", oldName, newName);
+        }
+
+        public async Task deleteMessage(string mainId)
+        {
+            await this.Clients.All.SendAsync("deleteMessage", mainId);
+        }
+
+    }
+    
+}
---
+++ WebChat/Startup.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebChat
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+        }
+    }
+}
